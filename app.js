1c2ef40bccd82ec156661df5f4545ec19be8305d
JaneneS23 <jsherrard4@gmail.com>
2016-03-04 09:56:32 -0800

    added function namePicker and variable to splice names form array

--- app.js
+++ app.js
@@ -4,3 +4,7 @@ function resetNames() {
 	CodetalkNames = ["Bernice", "Elliette", "Mirna", "Erin", "Aimee", "Yadira", "Jessica", "Janene", "Denise"]
 }
 
+function namePicker() {
+	var namePick = CodetalkNames.splice)(Math.floor(Math.random( * CodetalkNames.lenght), 1))
+
+}
